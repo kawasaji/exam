f2b3add9b240fa60f232ce101b4a01213a3990a0
kawasaji <106802228+kawasaji@users.noreply.github.com>
2022-12-09 11:34:13 +0400

    main2

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -3,31 +3,87 @@
 #include <string>
 #include <fstream>
 using namespace std;
+
+
+
+
+
 class creditCard
 {
 public:
 	creditCard()
 	{
-		system("cls");
-		cout << "--- Create new card ---\n\n[1] - credit card\n[2] - debit card\n(1|2)~# ";
-		cin >> typeOfCard;
-		system("cls");
-		cout << "--- Create new card ---\n\nenter cardholder name\n~# ";
-		cin >> ownerName;
-		system("cls");
-		cout << "--- Create new card ---\n\nenter cardholder surname\n~# ";
-		cin >> ownerSurname;
-		system("cls");
-		cout << "--- Create new card ---\n\nenter card`s number\n~# ";
-		cin >> cardNumber;
-		system("cls");
-		cout << "--- Create new card ---\n\nenter month expires\n~# ";
-		cin >> month;
-		system("cls");
-		cout << "--- Create new card ---\n\nenter year expires\n~# ";
-		cin >> month;
+		try
+		{
+			system("cls");
+			cout << "--- Create new card ---\n\n[0] - credit card\n[1] - debit card\n(0|1)~# ";
+			cin >> typeOfCard;
+	
+
+			system("cls");
+			cout << "--- Create new card ---\n\nenter cardholder name\n~# ";
+			cin >> ownerName;
+			
+
+			system("cls");
+			cout << "--- Create new card ---\n\nenter cardholder surname\n~# ";
+			cin >> ownerSurname;
+
+
+			system("cls");
+			cout << "--- Create new card ---\n\nenter card`s number, length must be 16\n~# ";
+			cin >> cardNumber;
+
+			if (cardNumber.length() < 16 || cardNumber.length() > 16)
+			{
+				throw exception("length error, length must be 16");
+			}
+			system("cls");
+			cout << "--- Create new card ---\n\nenter month expires\n~# ";
+			cin >> month;
+			system("cls");
+			cout << "--- Create new card ---\n\nenter year expires\n~# ";
+			cin >> month;
+		}
+		catch (exception& error)
+		{
+			cout << error.what() << endl;
+		}
+		
 
 	}
+
+	void print()
+	{
+		system("cls");
+		cout << char(218);
+		for (size_t i = 0; i < 30; i++)
+		{
+			cout << char(196);
+		} 
+		cout << char(191) << endl;
+		cout << "|                              |" << endl;
+		cout << "| Bank of baku               " << char(156) << " |" << endl;
+		cout << "|                              |" << endl;
+		cout << "|                              |" << endl;
+		cout << "| ";
+		for (size_t i = 0; i < cardNumber.length(); i++)
+		{
+			if (i % 4 == 0 && i != 0)
+			{
+				cout << ' ';
+			}
+			cout << cardNumber[i];
+			
+			
+		}
+		cout << "          |" << endl;
+		cout << "| ";
+		if (month < 10)
+		{
+			cout << "0" << month << "\\" << year;
+		}
+	}	
 
 private:
 	bool typeOfCard;
@@ -41,6 +97,10 @@ private:
 
 int main()
 {
+
 	creditCard lol{};
+
+	//lol.print();
+
 }
 
