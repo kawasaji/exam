8d578484f6eccb8641ac587e99ac447e634b5c03
kawasaji <106802228+kawasaji@users.noreply.github.com>
2022-12-14 12:04:41 +0400

    json update

---
+++ main/main/card.h
@@ -0,0 +1,148 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class creditCard
+{
+public:
+
+	creditCard() = default;
+
+	creditCard(int typeOfCard, string ownerName, string ownerSurname, string cardNumber, string month, string year)
+	{
+		this->typeOfCard = typeOfCard;
+		this->ownerName = ownerName;
+		this->ownerSurname = ownerSurname;
+		this->cardNumber = cardNumber;
+		this->month = month;
+		this->year = year;
+	}
+
+	void create()
+	{
+		try
+		{
+			system("cls");
+			cout << "--- Create new card ---\n\n[0] - credit card\n[1] - debit card\n(0|1)~# ";
+			cin >> typeOfCard;
+			if (cin.fail() || cin.bad())
+			{
+				throw exception("incorrect input");
+			}
+
+			system("cls");
+			cout << "--- Create new card ---\n\nenter cardholder name\n~# ";
+			cin >> ownerName;
+
+
+			system("cls");
+			cout << "--- Create new card ---\n\nenter cardholder surname\n~# ";
+			cin >> ownerSurname;
+
+
+			system("cls");
+			cout << "--- Create new card ---\n\nenter card`s number, length must be 16\n~# ";
+			cin >> cardNumber;
+
+			if (cardNumber.length() < 16 || cardNumber.length() > 16)
+			{
+				throw exception("length error, length must be 16");
+			}
+			system("cls");
+			cout << "--- Create new card ---\n\nenter month expires\n~# ";
+			cin >> month;
+			system("cls");
+			cout << "--- Create new card ---\n\nenter year expires\n~# ";
+			cin >> year;
+		}
+		catch (exception& error)
+		{
+			//cout << error.what() << endl;
+			throw error;
+		}
+	}
+
+	void print()
+	{
+		system("cls");
+		cout << char(218);
+		for (size_t i = 0; i < 30; i++)
+		{
+			cout << char(196);
+		}
+		cout << char(191) << endl;
+		cout << "|                              |";
+		if (typeOfCard == 0)
+		{
+			cout << "\tType - credit" << endl;
+		}
+		else
+		{
+			cout << "\tType - debit" << endl;
+		}
+		cout << "| Bank of baku               " << "X" << " |" << "\t" << ownerName << endl;
+		cout << "|                              |" << "\t" << ownerSurname << endl;
+		cout << "|                              |" << "\tBalance - " << balance << "$" << endl;
+		cout << "| ";
+		for (size_t i = 0; i < cardNumber.length(); i++)
+		{
+			if (i % 4 == 0 && i != 0)
+			{
+				cout << ' ';
+			}
+			cout << cardNumber[i];
+
+
+		}
+		cout << "          |" << endl;
+		cout << "| ";
+		int numm;
+		numm = stoi(month);
+		if (numm < 10)
+		{
+			cout << "0" << month << "\\" << year;
+		}
+		else
+		{
+			cout << month << "\\" << year;
+		}
+		cout << "                        |" << endl;
+		cout << "|                              |" << endl;
+		cout << char(192);
+		for (size_t i = 0; i < 30; i++)
+		{
+			cout << char(196);
+		}
+		cout << char(217) << endl;
+	}
+
+	void increase(int num)
+	{
+		system("cls");
+		cout << "Old balance - " << balance << "$" << endl;
+		this->balance += num;
+		cout << "New balance - " << balance << "$" << endl;
+	}
+
+	void decrease(int num)
+	{
+
+	}
+	int typeOfCard;
+	string ownerName;
+	string ownerSurname;
+	string cardNumber;
+	string month;
+	string year;
+	int balance = 0;
+private:
+	/*int typeOfCard;
+	string ownerName;
+	string ownerSurname;
+	string cardNumber;
+	string month;
+	string year;
+	int balance = 0;*/
+};
--- main/main/main.cpp
+++ main/main/main.cpp
@@ -2,154 +2,107 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <windows.data.json.h>
+#include "card.h"
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+//#include <nlohmann/json.hpp>
 using namespace std;
-
-
-
-
-
-class creditCard
-{
-public:
-	creditCard()
-	{
-		try
-		{
-			system("cls");
-			cout << "--- Create new card ---\n\n[0] - credit card\n[1] - debit card\n(0|1)~# ";
-			cin >> typeOfCard;
-	
-
-			system("cls");
-			cout << "--- Create new card ---\n\nenter cardholder name\n~# ";
-			cin >> ownerName;
-			
-
-			system("cls");
-			cout << "--- Create new card ---\n\nenter cardholder surname\n~# ";
-			cin >> ownerSurname;
-
-
-			system("cls");
-			cout << "--- Create new card ---\n\nenter card`s number, length must be 16\n~# ";
-			cin >> cardNumber;
-
-			if (cardNumber.length() < 16 || cardNumber.length() > 16)
-			{
-				throw exception("length error, length must be 16");
-			}
-			system("cls");
-			cout << "--- Create new card ---\n\nenter month expires\n~# ";
-			cin >> month;
-			system("cls");
-			cout << "--- Create new card ---\n\nenter year expires\n~# ";
-			cin >> year;
-		}
-		catch (exception& error)
-		{
-			cout << error.what() << endl;
-		}
-		
-
-	}
-
-	void print()
-	{
-		system("cls");
-		cout << char(218);
-		for (size_t i = 0; i < 30; i++)
-		{
-			cout << char(196);
-		} 
-		cout << char(191) << endl;
-		cout << "|                              |";
-		if (typeOfCard == 0)
-		{
-			cout << "\tType - credit" << endl;
-		}
-		else
-		{
-			cout << "\tType - debit" << endl;
-		}
-		cout << "| Bank of baku               " << "X" << " |" << "\t" << ownerName << endl;
-		cout << "|                              |" << "\t" << ownerSurname << endl;
-		cout << "|                              |" << "\tBalance - " << balance << "$" << endl;
-		cout << "| ";
-		for (size_t i = 0; i < cardNumber.length(); i++)
-		{
-			if (i % 4 == 0 && i != 0)
-			{
-				cout << ' ';
-			}
-			cout << cardNumber[i];
-			
-			
-		}
-		cout << "          |" << endl;
-		cout << "| ";
-		int numm;
-		numm = stoi(month);
-		if (numm < 10)
-		{
-			cout << "0" << month << "\\" << year;
-		}
-		else
-		{
-			cout << month << "\\" << year;
-		}
-		cout << "                        |" << endl;
-		cout << "|                              |" << endl;
-		cout << char(192);
-		for (size_t i = 0; i < 30; i++)
-		{
-			cout << char(196);
-		}
-		cout << char(217) << endl;
-	}	
-
-	void increase(int num)
-	{
-		system("cls");
-		cout << "Old balance - " << balance << "$" << endl;
-		this->balance += num;
-		cout << "New balance - " << balance << "$" << endl;
-	}
-
-	void decrease(int num)
-	{
-
-	}
-
-private:
-	bool typeOfCard;
-	string ownerName;
-	string ownerSurname;
-	string cardNumber;
-	string month;
-	string year;
-	int balance = 0;
-};
-
+using namespace rapidjson;
+//using json = nlohmann::json;
+//using namespace nlohmann::literals;
 
 int main()
 {
 
+	/*fstream file("notes.json");
+	json data = json::parse(file);*/
+
+	/*json ex3 = {
+		{"happy", true},
+		{"pi", 3.141},
+	};*/
+
+	/*Document doc;
+	fstream file("data.json", ios::in);
+	string json;*/
+
+	
+
 	int length = 3, k = 0;
 	creditCard* cards = new creditCard[length];
-	while (true)
+
+	// JSON
+
+	Document doc;
+	fstream file("notes.json", ios::in);
+	string json;
+
+	while (!file.eof())
 	{
+		string tmp;
+		file >> tmp;
 
-		cout << "[1] - Add new card"
-			<< "[2] - Update balance"
-			<< "[3] - Remove from balance"
-			<< endl;
-
+		json += tmp;
 	}
 
-	creditCard lol{};
+	doc.Parse(json.c_str());
 
-	lol.print();
+	StringBuffer buffer;
+	Writer<StringBuffer> writer(buffer);
+	doc.Accept(writer);
+
+	//cards[0].age = doc["age"].GetInt();
+	cards[0].ownerName = doc["name"].GetString();
+	cards[0].ownerSurname = doc["surname"].GetString();
+
+	// JSON
+
+
+	while (true)
+	{
+		cout << "press any key..." << endl;
+		cin.ignore();
+		int choice = 0;
+		cout << "[1] - Add new card\n"
+			<< "[2] - Print card\n"
+			<< "[3] - Update balance\n"
+			<< "[4] - Remove from balance\n"
+			<< "~# ";
+
+		cin >> choice;
+
+		if (choice == 1)
+		{
+			if (k > 3)
+			{
+				cout << "i can't add new card." << endl;
+			}
+			else
+			{
+				try
+				{
+					cards[k].create();
+				}
+				
+				
+				catch (exception& error)
+				{
+					cout << error.what() << endl;
+					//break;
+				}
+				k++;
+			}
+		}
+		else if (choice == 2)
+		{
+			int index = 0;
+			cout << "enter card`s index (1|2|3)\n~# ";
+			cin >> index;
+			cards[index].print();
+		}
+
+	}
 
 }
 
